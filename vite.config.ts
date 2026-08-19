e815ef3c415af4aeb9aeb5bfeba929c2b0638010
Kaesimo <kaio.felipe27@hotmail.com>
2026-08-19 16:56:41 -0300

    fix: add nitro for tanstack start vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from 'nitro/vite';
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart(), // This handles everything now
+    nitro(),
     react(),
     tailwindcss(),
   ],

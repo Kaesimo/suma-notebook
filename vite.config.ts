7f0d03dc53bd63837f1ef57531562f2b848ebca9
Kaesimo <kaio.felipe27@hotmail.com>
2026-08-19 17:03:54 -0300

    fix: remove pacotes inuteis e configura preset vercel no tanstack

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import nitro from 'nitropack/vite';
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart(), // This handles everything now
-    nitro(),
     react(),
     tailwindcss(),
   ],
